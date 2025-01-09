a225a5d9696e7d1eec1cd4b55fb02494c73f358e
ahonkazi <aohinuzzaman420@gmail.com>
2025-01-09 18:28:51 +0600

    server done

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+});
+
+io.on("connection", (socket) => {
+    console.log(`a user connected ${socket.id}`);
+
+    socket.on("send_message", (data) => {
+        console.log(data)
+        socket.emit("receive_message", data);
+    });
+
+    socket.on("notificationAccepted",()=>{
+
+        setInterval(()=>{
+            const content = {
+                title:"Hi brother",
+                message:"How are you?"
+            }
+            socket.emit("notificationSend",content)
+        },1000*60)
+    })
+
+
+});
+
+
+server.listen(4000, () => {
+    console.log("listening on *:4000");
+});
