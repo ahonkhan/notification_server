e048ffe16eaf8dec52a1ddee7a18f5b572e7e4d9
ahonkazi <aohinuzzaman420@gmail.com>
2025-01-09 19:09:58 +0600

    okk

--- server.js
+++ server.js
@@ -7,32 +7,28 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: { origin: "*", methods: ["GET", "POST"] },
+  cors: { origin: "*", methods: ["GET", "POST"] },
 });
 
 io.on("connection", (socket) => {
-    console.log(`a user connected ${socket.id}`);
+  console.log(`a user connected ${socket.id}`);
 
-    socket.on("send_message", (data) => {
-        console.log(data)
-        socket.emit("receive_message", data);
-    });
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_message", data);
+  });
 
-    socket.on("notificationAccepted",()=>{
-
-        setInterval(()=>{
-            const content = {
-                title:"Hi brother",
-                message:"How are you?"
-            }
-            socket.emit("notificationSend",content)
-        },1000*60)
-    })
-
-
+  socket.on("notificationAccepted", () => {
+    setInterval(() => {
+      const content = {
+        title: "Hi brother",
+        message: "How are you?",
+      };
+      socket.emit("notificationSend", content);
+    }, 1000 * 60);
+  });
 });
 
-
 server.listen(4000, () => {
-    console.log("listening on *:4000");
+  console.log("listening on *:4000");
 });
